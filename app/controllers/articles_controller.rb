f0bb9f707155ba12538c185521df68e683d374d4
tavo <gasb_150@hotmail.com>
2021-03-04 16:15:56 -0500

    Fix categoryes form otions& controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,12 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
     
+    
   end
 
   # GET /articles/1 or /articles/1.json
   def show
+    
   end
 
   # GET /articles/new
@@ -24,11 +26,11 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     
-    @article = current_user.articles.new(article_params, category_params, empty_params)
-    @category = Category.find_by(id: :category_ids)
-    respond_to do |format|      
+    @article = current_user.articles.new(article_params)
+    respond_to do |format|    
+     
       if @article.save
-          @article.categories << @category
+        
         format.html { redirect_to @article, notice: "Article was successfully created." }
         format.json { render :show, status: :created, location: @article }
       else
@@ -69,13 +71,7 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :text, :image_url)
-    end
-    def category_params
-      params[:product][:category_ids] ||= []
-    end
-    def empty_params
-      params[:product] ||= {}
+      params.require(:article).permit(:title, :text, :image_url, category_ids:[])
     end
 
 end
