8758343ee2c9e9d86f6acca8699997b12080d238
tavo <gasb_150@hotmail.com>
2021-03-04 17:47:40 -0500

    fix vote redirect

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,9 +3,9 @@ class VotesController < ApplicationController
     def create
         @vote = current_user.votes.new(article_id: params[:article_id])
         if @vote.save
-            redirect_to category_path(@vote.article), notice: "You vote to \"#{@vote.article.title}\" article"
+            redirect_to root_path,  notice: "You vote to \"#{@vote.article.title}\" article"
         else
-            redirect_to category_path(@vote.article), notice: "Sorry you can't vote to #{@vote.article.title}"
+            redirect_to root_path,  notice: "Sorry you can't vote to #{@vote.article.title}"
         end
     end
 
@@ -14,9 +14,9 @@ class VotesController < ApplicationController
         if vote
             article = vote.article.title
             vote.destroy
-            redirect_to category_path(vote.article), notice: "You unvote to \"#{article}\" article"
+            redirect_to root_path, notice: "You unvote to \"#{article}\" article"
           else
-            redirect_to category_path(vote.article), alert: 'You cannot dislike post that you did not like before.'
+            redirect_to root_path, alert: 'You cannot dislike post that you did not like before.'
           end
     end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,17 @@
 module ApplicationHelper
-
-    def vote_unvote_btn(article)
+def most_voted(most_voted)
+  if !most_voted.nil?
+    most_voted.title
+  end
+end
+    def vote_unvote_btn(article, category)
         vote = Vote.find_by(article: article, user: current_user)
         if vote
-          link_to('Unvote!', article_vote_path(id: vote.id, article_id: article.id), method: :delete)
+          link_to('Unvote!', article_vote_path(id: vote.id, article_id: article.id, category_id: category), method: :delete)
+        
         else
-          link_to('Vote!', article_votes_path(article_id: article.id, user_id:current_user), method: :post)
+          link_to('Vote!', article_votes_path(article_id: article.id, user_id: current_user.id, category_id: category), method: :post)
         end
     end
-    def category_list(article, category)
-      link_to('acept', articles_categories_path(category_id: category.id, article_id: article.id), method: :create)
-    end
+  
 end
