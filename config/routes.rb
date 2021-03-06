144217b1fea997e17a7472ec3aaa9cec899f2eb0
tavo <gasb_150@hotmail.com>
2021-03-06 12:47:43 -0500

    Add style user session, fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :articles_categories
-  resources :categories
-  resources :users
-  resources :articles  do
-  resources :votes, only: [:create, :destroy]
+  resources :categories, only: %i[index show new create]
+  resources :articles, only: %i[show new create edit destroy ]   do
+  resources :votes, only: %i[create destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "categories#index"
+
 end
