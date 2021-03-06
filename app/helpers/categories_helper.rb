eea16426c65e4f35972fced99ba1f8c123d2a0af
tavo <gasb_150@hotmail.com>
2021-03-05 23:09:48 -0500

    fix footer

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+    def category_nav
+        list =" "
+        Category.all.each do |category|
+         list += "<li> #{link_to category.name, category_path(category)} </li>"
+         list += " "
+      end
+      list.html_safe
+    end
 end
