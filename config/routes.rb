412448a421f69faae08bf4a5ae47f0e5ad9007d5
tavo <gasb_150@hotmail.com>
2021-03-07 17:09:01 -0500

    wrong uloading

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'articles', to: 'categories#index'
   devise_for :users
   resources :categories, only: %i[index show new create]
-  resources :articles, only: %i[index show new create ]   do
+  resources :articles, only: %i[index show new create edit]   do
   resources :votes, only: %i[create destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cat1 = Category.create!(
+    name: 'Action',
+    priority: '1',
+)
+
+cat2 = Category.create!(
+    name: 'Adventure',
+    priority: '5',
+)
+cat3 = Category.create!(
+    name: 'Role-playing',
+    priority: '3',
+)
+cat4 = Category.create!(
+    name: 'Simulation',
+    priority: '4',
+)
+cat5 = Category.create!(
+    name: 'Strategy',
+    priority: '7',
+)
+cat6 = Category.create!(
+    name: 'Sports',
+    priority: '6',
+)
+cat7 = Category.create!(
+    name: 'MMO',
+    priority: '2',
+)
