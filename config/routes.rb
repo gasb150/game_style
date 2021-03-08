b1939e43d4a2eeebe86909a6dd74d3378c92e39a
tavo <gasb_150@hotmail.com>
2021-03-08 00:03:49 -0500

    restore db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users', to: 'categories#index'
-  get 'users/:id', to: 'categories#index'
+  get 'users/sing_up', to: 'users#new'
   devise_for :users
   resources :categories, only: %i[index show new create]
   resources :articles, only: %i[index show new create edit]   do
--- spec/feature/article_spec.rb
+++ spec/feature/article_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'create new article', type: :feature do
+RSpec.describe 'create new article with/without info', type: :feature do
  @category = Category.create(name:"Action", priority:1)
  
  before(:each) do
@@ -60,14 +60,24 @@ end
     click_on 'Create Article'
     expect(page).to have_content('Please review the problems below:')
   end
+end
+RSpec.describe 'create new article without user', type: :feature do
   scenario 'Create article with unvalid user' do
-    visit new_user_registration_path
-    fill_in 'Name', with: 'gustavo'
-    fill_in 'Username', with: 'gustavo'
-    fill_in 'Email', with: 'gustavo@email.com'
-    fill_in 'Password', with: '123456'
-    fill_in 'Confirm your password', with: '123456'
-    click_on 'Sign up'
+    
+     visit new_user_registration_path
+     fill_in 'Name', with: 'gustdavo'
+     fill_in 'Username', with: 'gustavo'
+     fill_in 'Email', with: 'gusstavo@email.com'
+     fill_in 'Password', with: ''
+     fill_in 'Confirm your password', with: '123456'
+     click_on 'Sign up'
+     visit new_article_path
+     fill_in 'Title', with: 'test-title'
+     fill_in 'Text', with: 'test-text'
+     check "category_1"
+     click_on 'Create Article'
+     expect(page).to have_content('Hello')
+  end
 
 
 end
